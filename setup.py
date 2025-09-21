1cf09c350ae84e1e7222d8fa2b4fddccb5ab34fd
federicorabanos <lanusstats@gmail.com>
2025-09-21 19:28:35 -0300

    version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 HERE = pathlib.Path(__file__).parent
 
 
-VERSION = '1.8.4'
+VERSION = '1.8.5'
 PACKAGE_NAME = 'LanusStats'
 AUTHOR = 'Federico Rábanos'
 AUTHOR_EMAIL = 'lanusstats@gmail.com'
